4ff3aef4517f2341d7ed07f1a71f3c312c17b107
dongwang881107 <dongwang@seu.edu.cn>
2021-12-16 15:27:16 +0800

    parser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 import torch.optim as optim
 
@@ -8,8 +9,8 @@ from model import *
 from loss import *
 
 def main(args):
-    # set seed and type
-    torch.manual_seed(1000)
+    # set seed and data type
+    torch.manual_seed(args.seed)
     torch.set_default_tensor_type('torch.DoubleTensor')
 
     # create folder if not exist
@@ -17,51 +18,70 @@ def main(args):
         os.makedirs(args.save_path)
         print('Create path : {}'.format(args.save_path))
 
-    # generate supervised data
-    supervised_dataset = SupervisedPoints(args.num_supervised_points, args.lower, args.upper)
-
-    # generate networks
-    model_u = ResNet(args.num_channels, args.num_blocks)
-    model_f = ResNet(args.num_channels, args.num_blocks)
-
-    # loss function and optimizer
-    criterion = compute_loss
-    optimizer = optim.Adam([{'params':model_u.parameters(),'lr':args.lr}, {'params':model_f.parameters(),'lr':args.lr}])
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.decay_iters, gamma=args.gamma)
-
-    # build solver
-    solver = Solver(supervised_dataset, model_u, model_f, criterion, optimizer, scheduler, args)
-
     # training/testing
     if args.mode == 'train':
+        # generate supervised data
+        supervised_dataset = SupervisedPoints(args.num_supervised_points, args.lower, args.upper)
+        # generate networks
+        model_u = ResNet(args.num_channels, args.num_blocks).to(args.device)
+        model_f = ResNet(args.num_channels, args.num_blocks).to(args.device)
+        # loss function and optimizer
+        criterion = compute_loss
+        optimizer = optim.Adam([{'params':model_u.parameters(),'lr':args.lr}, {'params':model_f.parameters(),'lr':args.lr}])
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.decay_iters, gamma=args.gamma)
+        # build solver
+        args.criterion = criterion
+        args.optimizer = optimizer
+        args.scheduler = scheduler
+        solver = Solver(supervised_dataset, model_u, model_f, args)
+        # training
         solver.train()
-    elif args.mode == 'test':
-        solver.test()
     else:
-        print('train | test')
+        # generate testing data
+        testing_dataset = SupervisedPoints(args.num_testing_points, 0, 1)
+        # load networks
+        model_u = ResNet(args.num_channels, args.num_blocks).to(args.device)
+        model_f = ResNet(args.num_channels, args.num_blocks).to(args.device)
+        model_u_path = os.path.join(args.save_path, args.model_name+'_u.pkl')
+        model_f_path = os.path.join(args.save_path, args.model_name+'_f.pkl')
+        model_u.load_state_dict(torch.load(model_u_path))
+        model_f.load_state_dict(torch.load(model_f_path))
+        # bulid solver
+        solver = Solver(testing_dataset, model_u, model_f, args)
+        # testing
+        solver.test()
 
 if __name__ == "__main__":
     # parse arguments
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='DL-EIT')
+    subparsers = parser.add_subparsers(dest = 'mode', required=True, help='train | test')
 
-    parser.add_argument('--mode', type=str, default='train', help='train | test')
-    parser.add_argument('--num_interior_points', type=int, default=1000, help='number of interior points inside the domain Omega')
-    parser.add_argument('--num_boundary_points', type=int, default=100, help='number of points on the boundary partial Omega')
-    parser.add_argument('--num_supervised_points', type=int, default=1000, help='number of supervised points inside the domain Omega_0')
-    parser.add_argument('--lower', type=float, default=0.5, help='lower bound of Omega_0')
-    parser.add_argument('--upper', type=float, default=0.75, help='upper bound of Omega_0')
+    # shared paramters
+    parser.add_argument('--device', type=str, default='cpu', help='cpu | cuda')
+    parser.add_argument('--seed', type=int, default=1000, help='random seed')
     parser.add_argument('--num_channels', type=int, default=6, help='hidden layer width of network')
     parser.add_argument('--num_blocks', type=int, default=3, help='number of residual blocks of network')
-    parser.add_argument('--lambda1', type=float, default=1, help='hyper-parameter for the boundary loss')
-    parser.add_argument('--lambda2', type=float, default=100, help='hyper parameter for the supervised loss')
-    parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
-    parser.add_argument('--decay_iters', type=int, default=1000, help='number of iterations to decay learning rate')
-    parser.add_argument('--gamma', type=float, default=0.5, help='decay value of the learning rate')
-    parser.add_argument('--device', type=str, help='cpu | cuda')
-    parser.add_argument('--num_epochs', type=int, default=20000, help='number of epochs')
-    parser.add_argument('--print_iters', type=int, default=100, help='number of iterations to print statistics')
-    parser.add_argument('--save_path', type=str, default='./result', help='path of result')
-    parser.add_argument('--model_name', type=str, default='model', help='name of the output model')
+    parser.add_argument('--save_path', type=str, default='./result', help='saved path of the results')
+    parser.add_argument('--model_name', type=str, default='model', help='name of the models')
+    # training parameters
+    subparser_train = subparsers.add_parser('train', help='training mode')
+    subparser_train.add_argument('--mode', type=str, default='train', help='train | test')
+    subparser_train.add_argument('--num_interior_points', type=int, default=1000, help='number of interior points inside the domain Omega')
+    subparser_train.add_argument('--num_boundary_points', type=int, default=100, help='number of points on the boundary partial Omega')
+    subparser_train.add_argument('--num_supervised_points', type=int, default=1000, help='number of supervised points inside the domain Omega_0')
+    subparser_train.add_argument('--lower', type=float, default=0.5, help='lower bound of Omega_0')
+    subparser_train.add_argument('--upper', type=float, default=0.75, help='upper bound of Omega_0')
+    subparser_train.add_argument('--lambda1', type=float, default=1, help='hyper-parameter for the boundary loss')
+    subparser_train.add_argument('--lambda2', type=float, default=100, help='hyper parameter for the supervised loss')
+    subparser_train.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+    subparser_train.add_argument('--decay_iters', type=int, default=1000, help='number of iterations to decay learning rate')
+    subparser_train.add_argument('--gamma', type=float, default=0.5, help='decay value of the learning rate')
+    subparser_train.add_argument('--num_epochs', type=int, default=20000, help='number of epochs')
+    subparser_train.add_argument('--print_iters', type=int, default=100, help='number of iterations to print statistics')
+    # testing parameters
+    subparser_test = subparsers.add_parser('test', help='testing mode')
+    subparser_test.add_argument('--mode', type=str, default='test', help='train | test')
+    subparser_test.add_argument('--num_testing_points', type=int, default=1000, help='number of testing points')
 
     args = parser.parse_args()
 
--- solver.py
+++ solver.py
@@ -4,30 +4,37 @@ import numpy as np
 import time
 
 from data import *
+from measure import *
 
 class Solver(object):
-    def __init__(self, supervised_dataset, model_u, model_f, criterion, optimizer, scheduler, args):
+    def __init__(self, dataset, model_u, model_f, args):
         super().__init__()
-        if args.device:
-            self.device = torch.device(args.device)
-        else:
-            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        self.supervised_dataset = supervised_dataset
+        self.device = torch.device(args.device)
+
+        self.dataset = dataset
         self.model_u = model_u
         self.model_f = model_f
-        self.model_u.to(self.device)
-        self.model_f.to(self.device)
+        
+        # move supervised_points/solutions to self.device
+        s = self.dataset.points
+        us_exact = self.dataset.solutions
+        s = s.to(self.device)
+        us_exact = us_exact.to(self.device)
+        self.s = s
+        self.us_exact = us_exact
 
-        self.criterion = criterion
-        self.optimizer = optimizer
-        self.scheduler = scheduler
+        if args.mode == 'train':
+            self.criterion = args.criterion
+            self.optimizer = args.optimizer
+            self.scheduler = args.scheduler
 
-        self.mode = args.mode
-        self.num_interior_points = args.num_interior_points
-        self.num_boundary_points = args.num_boundary_points
-        self.num_epochs = args.num_epochs
-        self.print_iters = args.print_iters
+            self.num_interior_points = args.num_interior_points
+            self.num_boundary_points = args.num_boundary_points
+            self.num_epochs = args.num_epochs
+            self.print_iters = args.print_iters
+        else:
+            self.num_testing_points = args.num_testing_points
         
         self.save_path = args.save_path
         self.model_name = args.model_name
@@ -73,12 +80,6 @@ class Solver(object):
     def train(self):
         start_time = time.time()
         training_loss = []
-        
-        # move supervised_points/solutions to self.device
-        s = self.supervised_dataset.points
-        us_exact = self.supervised_dataset.solutions
-        s = s.to(self.device)
-        us_exact = us_exact.to(self.device)
 
         print('Training start!')
         for epoch in range(self.num_epochs):
@@ -103,10 +104,10 @@ class Solver(object):
             # forward propagation
             ux_pred = self.model_u(x)
             ub_left_pred, ub_right_pred, ub_bottom_pred, ub_top_pred = self.propagate_boundary(b_left, b_right, b_bottom, b_top)
-            us_pred = self.model_u(s)
+            us_pred = self.model_u(self.s)
             f_pred = self.model_f(x)
             # compute loss
-            loss = self.criterion(x, b_left, b_right, b_bottom, b_top, s, ux_pred, ub_left_pred, ub_right_pred, ub_bottom_pred, ub_top_pred, us_pred, us_exact, f_pred, self.args)
+            loss = self.criterion(x, b_left, b_right, b_bottom, b_top, self.s, ux_pred, ub_left_pred, ub_right_pred, ub_bottom_pred, ub_top_pred, us_pred, self.us_exact, f_pred, self.args)
             # backward propagation
             loss.backward()
             # optimize
@@ -128,5 +129,18 @@ class Solver(object):
     
     # test
     def test(self):
-        print('Done!')
+        print('Testing start!')
+        with torch.no_grad():
+            ux_pred = self.model_u(self.s)
+            f_pred = self.model_f(self.s)
+            f_exact = f(self.s).to(self.device)
+            u_relative_error, u_l2_error = compute_measure(ux_pred, self.us_exact) 
+            f_relative_error, f_l2_error = compute_measure(f_pred, f_exact) 
+
+        print('Relative error of u is {:.4f}'.format(u_relative_error))
+        print('L2 error of u is {:.4f}'.format(u_l2_error))
+        print('Relative error of f is {:.4f}'.format(f_relative_error))
+        print('L2 error of f is {:.4f}'.format(f_l2_error))
+
+        print('Testing finished!')
             
